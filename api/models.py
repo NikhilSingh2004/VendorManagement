76fd197a6caf0f44cb1c722df88c4a04928f5eb5
Nikhil Singh <nikhilsinghcr007@gmail.com>
2024-05-11 00:11:36 +0530

    Model Creation

--- api/models.py
+++ api/models.py
@@ -1,8 +1,57 @@
 from django.db import models
+from django.conf import settings
+from django.contrib.auth.models import AbstractUser
+# from django.contrib.postgres.fields import JSONField
 
 # Vendor Model 
 
+class Vendor(AbstractUser):
+    
+    name = models.CharField(max_length=255, null=True, blank=True)
+    phone_number = models.CharField(max_length=15, null=True, blank=True)
+    email = models.CharField(max_length=256, null=True)
+    address = models.TextField(null=True, blank=True)
+    vendor_code = models.CharField(max_length=50, unique=True, null=True, blank=True)
+    on_time_delivery_rate = models.FloatField(null=True, blank=True)
+    quality_rating_avg = models.FloatField(null=True, blank=True)
+    average_response_time = models.FloatField(null=True, blank=True)
+    fulfillment_rate = models.FloatField(null=True, blank=True)
+    deleted = models.BooleanField(default=False)
 
+    class Meta(AbstractUser.Meta):
+        swappable = 'AUTH_USER_MODEL'
 
+    def __str__(self):
+        return self.name
+    
 # Purchase Order Model
 
+class PurchaseOrder(models.Model):
+    po_number = models.CharField(max_length=50, unique=True)
+    # vendor = models.ForeignKey(Vendor, on_delete=models.CASCADE, related_name='purchase_orders')
+    vendor = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='purchase_orders')
+    order_date = models.DateTimeField()
+    delivery_date = models.DateTimeField()
+    # items = JSONField()
+    quantity = models.IntegerField()
+    status = models.CharField(max_length=50)
+    quality_rating = models.FloatField(null=True, blank=True)
+    issue_date = models.DateTimeField()
+    acknowledgment_date = models.DateTimeField(null=True, blank=True)
+    
+    def __str__(self):
+        return self.po_number
+
+# Historical Performance
+
+class PerformanceRecord(models.Model):
+    # vendor = models.ForeignKey(Vendor, on_delete=models.CASCADE, related_name='performance_records')
+    vendor = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='performance_records')
+    date = models.DateTimeField()
+    on_time_delivery_rate = models.FloatField()
+    quality_rating_avg = models.FloatField()
+    average_response_time = models.FloatField()
+    fulfillment_rate = models.FloatField()
+
+    def __str__(self):
+        return f"Performance Record for {self.vendor.name} on {self.date}"
